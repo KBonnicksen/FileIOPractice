35c2a8d5b3eadbe580196cdfa3eba9ab71f410e0
KBonnicksen <kelseybon@gmail.com>
2019-08-22 12:31:46 -0700

    Add project files.

---
+++ FileIOPractice/Form1.Designer.cs
@@ -0,0 +1,71 @@
+﻿namespace FileIOPractice
+{
+    partial class frmImportFile
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnImport = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // btnImport
+            // 
+            this.btnImport.Location = new System.Drawing.Point(94, 121);
+            this.btnImport.Name = "btnImport";
+            this.btnImport.Size = new System.Drawing.Size(144, 40);
+            this.btnImport.TabIndex = 0;
+            this.btnImport.Text = "Import file";
+            this.btnImport.UseVisualStyleBackColor = true;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(45, 68);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(239, 22);
+            this.textBox1.TabIndex = 1;
+            // 
+            // frmImportFile
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(338, 199);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.btnImport);
+            this.Name = "frmImportFile";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnImport;
+        private System.Windows.Forms.TextBox textBox1;
+    }
+}
+
